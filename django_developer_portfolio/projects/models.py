e24d93cc4502d28c31a2356a3f24e27dea4dd49f
pythonredhat <pythonredhat1987@gmail.com>
2019-09-07 07:42:55 -0400

    Add projects model

--- django_developer_portfolio/projects/models.py
+++ django_developer_portfolio/projects/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.FilePathField(path="/img")
+
