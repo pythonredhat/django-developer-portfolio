9f3939279b92b0c342e3902892962d7aebacc1e9
pythonredhat <pythonredhat1987@gmail.com>
2019-09-07 08:11:23 -0400

    Add base html for bootstrap and link project settings to project templates

--- django_developer_portfolio/django_developer_portfolio/settings.py
+++ django_developer_portfolio/django_developer_portfolio/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_developer_portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["django_developer_portfolio/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
