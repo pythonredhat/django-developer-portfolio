2a640ad9d5258e3bd4802c720133aff474f5bf31
pythonredhat <pythonredhat1987@gmail.com>
2019-09-10 16:42:02 -0400

    Configure model, install semver

--- django_developer_portfolio/version_lord/models.py
+++ django_developer_portfolio/version_lord/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+import semantic_version
+
+class Version(models.Model):
+    software = models.CharField(max_length=255)
+    current_version = semantic_version.django_fields.SemVerField()
+    last_updated = models.DateTimeField(auto_now=True)
+
+
