7092205758a90276ab284fe04fcd73c3ddb9f505
pythonredhat <pythonredhat1987@gmail.com>
2019-09-10 16:50:01 -0400

    Adjust current version to a simple string, migrate databases

--- django_developer_portfolio/version_lord/models.py
+++ django_developer_portfolio/version_lord/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-import semantic_version
-
 class Version(models.Model):
     software = models.CharField(max_length=255)
-    current_version = semantic_version.django_fields.SemVerField()
+    current_version = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now=True)
+
+
 
 
