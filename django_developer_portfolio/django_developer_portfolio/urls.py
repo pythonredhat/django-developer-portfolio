cdb210222755d39bb8c6d8f6f5584f334a0e80d4
pythonredhat <pythonredhat1987@gmail.com>
2019-09-11 16:33:08 -0400

    Add verion_lord url to global project urls

--- django_developer_portfolio/django_developer_portfolio/urls.py
+++ django_developer_portfolio/django_developer_portfolio/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include("projects.urls")),
     path('blog/', include("blog.urls")),
+    url(r'^', include('version_lord.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
