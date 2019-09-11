30350f974eb10f4323b002ca6c02942abe426b7b
pythonredhat <pythonredhat1987@gmail.com>
2019-09-11 16:21:47 -0400

    Add skeleton for views api

--- django_developer_portfolio/version_lord/views.py
+++ django_developer_portfolio/version_lord/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Version
+from .serializers import VersionSerializer
+
+@api_view(['PUT'])
+def updated_version(request, pk):
+    try:
+        version = Version.objects.get(pk=pk)
+    except Version.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    #update details of a single version
+    if request.method == 'PUT':
+        return Response({})
+
+@api_view(['GET', 'POST'])
+def get_post_version(request):
+    #get all versions
+    if request.method == 'GET':
+        return Response({})
+    #insert a new version for a piece of software
+    elif request.method == 'POST':
+        return Response({})
