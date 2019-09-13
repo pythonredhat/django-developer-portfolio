3e7eef8d725a2f598bffb4f0ca6c8afb442125f8
pythonredhat <pythonredhat1987@gmail.com>
2019-09-13 12:57:51 -0400

    Add comments to entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+#halt on any error
+set -e
+
+#all env variables inherited from docker-compose
+#wait for database to come up
 if [ "$DATABASE" = "postgres" ]
 then 
   echo "Waiting for postgres..."
@@ -11,6 +16,10 @@ then
   echo "PostgreSQL started"
 fi
 
+#reset database
 python ./django_developer_portfolio/manage.py flush --no-input
+#migrate database
 python ./django_developer_portfolio/manage.py migrate
+
+#It does nothing except pass control back to whatever process is ran in your CMD instruction in your Dockerfile. That’s what exec "$@" does.
 exec "$@"
