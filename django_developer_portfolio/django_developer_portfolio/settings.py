2d34bbf4fa1dab297ecb712caa28dd495f92cc06
pythonredhat <pythonredhat1987@gmail.com>
2019-09-13 10:52:38 -0400

    Add postgres db config in settings

--- django_developer_portfolio/django_developer_portfolio/settings.py
+++ django_developer_portfolio/django_developer_portfolio/settings.py
@@ -79,10 +79,23 @@ WSGI_APPLICATION = 'django_developer_portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+'''original database settings for sqlite3
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.eviron.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.eviron.get('SQL_USER', 'user'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
+        'HOST': os.eviron.get('SQL_HOST', 'localhost'),
+        'PORT': os.eviron.get('SQL_PORT', '5432'),
     }
 }
 
