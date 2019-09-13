9cbfd4b2b35a8a38921d2eec8ccb60ce591963da
pythonredhat <pythonredhat1987@gmail.com>
2019-09-13 12:03:08 -0400

    Fix syntax error with database config in settings

--- django_developer_portfolio/django_developer_portfolio/settings.py
+++ django_developer_portfolio/django_developer_portfolio/settings.py
@@ -91,11 +91,11 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.eviron.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
-        'USER': os.eviron.get('SQL_USER', 'user'),
+        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get('SQL_USER', 'user'),
         'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
-        'HOST': os.eviron.get('SQL_HOST', 'localhost'),
-        'PORT': os.eviron.get('SQL_PORT', '5432'),
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get('SQL_PORT', '5432'),
     }
 }
 
