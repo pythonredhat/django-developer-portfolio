d9f80dc4a3dcb23479fd4503e612d39a6b822f68
pythonredhat <pythonredhat1987@gmail.com>
2019-09-22 07:14:30 -0400

    Add variables to handler function

--- django_developer_portfolio/blog/management/commands/populatedb.py
+++ django_developer_portfolio/blog/management/commands/populatedb.py
@@ -6,28 +6,28 @@ from django.contrib.auth.models import User
 
 class Command(BaseCommand): 
     help = 'Populates the postgresql database with fake blog data'
-    
-    c1 = Category(
-       name='web'
-    )
-
-    c2 = Category(
-       name='big data'
-    )
-
-    p1 = Post(
-       title="Web post test",
-       body="A fake blog post for the web",
-       #categories="web"
-    )
-
-    p2 = Post(
-       title="Big data post test",
-       body="A fake blog post for big data",
-       #categories="big data"
-    )
 
     def handle(self, *args, **options):
+      c1 = Category(
+        name='web'
+        )
+
+      c2 = Category(
+        name='big data'
+        )
+
+      p1 = Post(
+        title="Web post test",
+        body="A fake blog post for the web",
+        #categories="web"
+        )
+
+      p2 = Post(
+        title="Big data post test",
+        body="A fake blog post for big data",
+        #categories="big data"
+        )
+           
        c1.save()
        c2.save()
        p1.save()
